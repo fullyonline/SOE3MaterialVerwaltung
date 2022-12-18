996f6dd4d20374ae04fae962617531232db61a26
fullyonline <no.reply4me@gmail.com>
2022-12-18 11:05:09 +0100

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SOE3MaterialVerwaltung.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
