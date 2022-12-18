41a3f57ea777aa959538bb937bca5076c4932c44
fullyonline <no.reply4me@gmail.com>
2022-12-18 15:17:25 +0100

    added the needed connectionstring

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using SOE3MaterialVerwaltung.Models;
 using System.Data.Entity;
+using System.Reflection.Emit;
 
 namespace SOE3MaterialVerwaltung.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext()
+        {
+            this.Database.Connection.ConnectionString = @"Data Source=DESKTOP-VUM307Q\SQLEXPRESS;Initial Catalog=SOE3MaterialVerwaltung;Integrated Security=True";
+        }
         public DbSet<Material> Materials { get; set; }
         public DbSet<Materialgroup> Materialgroups { get; set; }
     }
