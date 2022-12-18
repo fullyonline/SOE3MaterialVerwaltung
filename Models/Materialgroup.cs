84a10f0a67c9ecde7484995ad5c1128b3d0f7d6b
fullyonline <no.reply4me@gmail.com>
2022-12-18 11:24:44 +0100

    added Materialgroup class

---
+++ Models/Materialgroup.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOE3MaterialVerwaltung.Models
+{
+    public class Materialgroup
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
