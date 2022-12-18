68529506d208df6d22f7abb2c1545397c5764289
fullyonline <no.reply4me@gmail.com>
2022-12-18 11:24:58 +0100

    added the ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using SOE3MaterialVerwaltung.Models;
+using System.Data.Entity;
+
+namespace SOE3MaterialVerwaltung.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Materialgroup> Materialgroups { get; set; }
+    }
+}
