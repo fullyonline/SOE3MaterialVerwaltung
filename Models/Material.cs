a5b9491c5c9066e7ae2912b56bd21c86bc890f6b
fullyonline <no.reply4me@gmail.com>
2022-12-18 11:24:31 +0100

    added Material class

---
+++ Models/Material.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOE3MaterialVerwaltung.Models
+{
+    public class Material
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        [Range(0, 9999999999999999.99)]
+        public decimal BuyPrice { get; set; }
+        [Range(0, 9999999999999999.99)]
+        public decimal SellPrice { get; set; }
+    }
+}
